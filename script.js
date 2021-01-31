0d968ae09fe2cb059b2630de8213ba076966709c
Yuriy Lipchey <xaosmmm@gmail.com>
2021-01-31 15:40:13 +0200

    project created

---
+++ script.js
@@ -0,0 +1,11 @@
+function print(variable) {
+    let c = 3
+    return function func(variable2) {
+        console.log(variable)
+        console.log(variable2)
+        console.log(c)
+    }
+}
+
+let a = print(1)
+a(2)
